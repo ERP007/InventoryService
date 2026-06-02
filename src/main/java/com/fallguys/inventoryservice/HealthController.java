18ada55cebeebd9130d4529124aaddef8250f934
Sangmin Kang <kkang5430@naver.com>
2026-06-02 12:01:59 +0900

    setting(ERP-00): dockerfile ã 수정, ãhealthcontroller (#0)

---
+++ src/main/java/com/fallguys/inventoryservice/HealthController.java
@@ -0,0 +1,14 @@
+package com.fallguys.inventoryservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/inventory")
+public class HealthController {
+    @GetMapping("/health")
+    String health() {
+        return "user-service ok";
+    }
+}
