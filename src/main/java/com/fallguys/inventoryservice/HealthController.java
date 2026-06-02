3bf88e269bfea40d1ce2df9ca083d217c27023c5
Sangmin Kang <kkang5430@naver.com>
2026-06-02 14:46:34 +0900

    setting(ERP-00): health check api  주소 수정 (#0)

--- src/main/java/com/fallguys/inventoryservice/HealthController.java
+++ src/main/java/com/fallguys/inventoryservice/HealthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/inventory")
+@RequestMapping("/inventory")
 public class HealthController {
     @GetMapping("/health")
     String health() {
